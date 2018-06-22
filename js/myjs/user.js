76c23cf77060a5370fcdd837bd2ed665cdd4624d
stew <stewforani@gmail.com>
2018-06-22 11:29:16 +0800

    add

--- js/myjs/user.js
+++ js/myjs/user.js
@@ -34,42 +34,36 @@ define(function(require, exports, module) {
 
         for (var i = 0; i < l; i++) {
             var item = d[i];
-            var user_list_item = '<div id="table_user">' +
-                '<tr class="bg-white">' +
-
-                '<th scope="row">' +
-                '<div class="media align-items-center">' +
-                '<img alt="Image" src="' + ACE_BASE_IMG_URL + item.avatar + '" class="avatar">' +
-                '</div>' +
+            var user_list_item =
+                ' <tr class="bg-white">' +
+                ' <th scope="row" style="width:200px">' +
+                '     <div class="media align-items-center">' +
+                '         <img alt="Image" src="' + ACE_BASE_IMG_URL + item.avatar + '" class="avatar">' +
+                '    </div>' +
                 '</th>' +
-
-                '<td>' + item.id + '</td>' +
-
+                ' <td>' + item.id + '</td>' +
                 '<td>' +
-                '<div class="media-body"><span class="h6 mb-0">' + item.username + '<span class="badge badge-secondary">Admin</span>' + '</span>' +
-                '</div>' +
+                '   <div class="media-body">' +
+                '      <span class="h6 mb-0">' + item.username +
+                '    </span>' +
+                '   </div>' +
+                '        </td>' +
+                '     <td>' + item.number + '</td>' +
+                '         <td>' + item.position + '</td>' +
+                '         <td>' +
+                ' <div class="dropdown">' +
+                '    <button class="btn btn-sm btn-outline-primary dropdown-toggle dropdown-toggle-no-arrow" type="button" id="dropdownMenuButton-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' +
+                '       <i class="icon-dots-three-horizontal"></i>' +
+                '  </button>' +
+                ' <div class="dropdown-menu dropdown-menu-sm" aria-labelledby="dropdownMenuButton" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 30px, 0px); top: 0px; left: 0px; will-change: transform;">' +
+                '  <a class="dropdown-item">Edit</a>' +
+                ' <div class="dropdown-divider"></div>' +
+                '<a class="dropdown-item">Delete</a>' +
+                ' </div>' +
+                '  </div>' +
                 '</td>' +
-
-                '<td>' + item.number + '</td>' +
-                '<td>' + item.position + '</td>' +
-
-                '<td>' +
-                '<div class="dropdown">' +
-                '<button class="btn btn-sm btn-outline-primary dropdown-toggle dropdown-toggle-no-arrow" type="button" id="dropdownMenuButton-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">' +
-                '<i class="icon-dots-three-horizontal"></i>' +
-                '</button>' +
-                '<div class="dropdown-menu dropdown-menu-sm" aria-labelledby="dropdownMenuButton" x-placement="bottom-start" style="position: absolute; transform: translate3d(0px, 30px, 0px); top: 0px; left: 0px; will-change: transform;">' +
-                '<a class="dropdown-item" href="#">Edit</a>' +
-                '<div class="dropdown-divider"></div>' +
-                '<a class="dropdown-item" href="#">Delete</a>' +
-                '</div>' +
-                '</div>' +
-                '</td>' +
-
-
-                '</tr>' +
-                '<tr class="table-divider"></tr>' +
-                '</div>';
+                '  </tr>' +
+                '  <tr class="table-divider"></tr>';
             $('#user_list').append(user_list_item);
         }
 
