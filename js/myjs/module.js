707b159a6cc18f9237e8a9b8a3682df335d242f3
stew <stewforani@gmail.com>
2018-06-22 19:02:34 +0800

    add

--- js/myjs/module.js
+++ js/myjs/module.js
@@ -1,5 +1,20 @@
-$(document).ready(function() {
+define(function(require, exports, module) {
+    require('bootstrap');
+    require('jquery');
+    require('ajaxUtil');
+    require('aceApiTool');
+    require('commoncontent');
 
+    var containerId = "#module_container";
+    
+    init();
 
+    function init() {
+        $("#log_out").click(function() {
+            logout();
+            return false;
+        });
+    }
 
+  
 });
